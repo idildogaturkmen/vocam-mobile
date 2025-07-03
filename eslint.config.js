eb40c26c6c1803c7111a5a07cc8c08515c279403
Matheus <cruzmatheus@gmail.com>
2025-07-03 20:55:20 -0300

    Configure vscode and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,41 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+import typescript from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
 
-module.exports = defineConfig([
-  expoConfig,
+export default [
   {
-    ignores: ['dist/*'],
+    files: ['**/*.ts', '**/*.tsx'],
+    ignores: ['node_modules', 'dist', 'build', 'android', 'ios'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json', // Se você usar tsconfig
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      prettier,
+    },
+    rules: {
+      // Regras TypeScript opcionais
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+
+      // Regras Prettier
+      'prettier/prettier': [
+        'error',
+        {
+          useTabs: false,
+          tabWidth: 4,
+          singleQuote: true,
+          semi: true,
+          endOfLine: 'auto',
+          printWidth: 100,
+          trailingComma: 'all',
+        },
+      ],
+    },
   },
-]);
+];

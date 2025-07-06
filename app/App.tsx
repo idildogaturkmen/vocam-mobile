205473fdef1d82e84a82653e94ef2b101e80fd1b
Matheus <cruzmatheus@gmail.com>
2025-07-06 14:37:11 -0300

    Adding a initial page with login-signup and a settings screen with a logout button

---
+++ app/App.tsx
@@ -0,0 +1,53 @@
+import 'react-native-url-polyfill/auto';
+import { useState, useEffect, use } from 'react';
+import { supabase } from '../database/config';
+import Auth from '../components/Auth/Auth';
+import { View, Text } from 'react-native';
+import { Session } from '@supabase/supabase-js';
+import { useRouter, useSegments } from 'expo-router';
+
+export default function App() {
+    const [session, setSession] = useState<Session | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const router = useRouter();
+    const segments = useSegments();
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session);
+            setIsLoading(false);
+        });
+
+        const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+            setSession(session);
+            setIsLoading(false);
+        });
+
+        return () => {
+            authListener.subscription?.unsubscribe();
+        };
+    }, []);
+
+    useEffect(() => {
+        if (isLoading) return;
+
+        const inAuthGroup = segments[0] === '(auth)';
+
+        if (session && !inAuthGroup) {
+            router.replace('/(tabs)/detection');
+        } else if (!session && inAuthGroup) {
+            router.replace('/App');
+        }
+    }, [session, isLoading, segments]);
+
+    if (isLoading) {
+        return null;
+    }
+
+    return (
+        <View>
+            <Auth />
+            {session && session.user && <Text>{session.user.id}</Text>}
+        </View>
+    );
+}
--- database/login.ts
+++ database/login.ts
@@ -1,14 +1,28 @@
+import { Alert } from 'react-native';
 import { supabase } from './config';
 import { writeUserData } from './crudOperations';
 
-export async function createUser(email: string, password: string, username: string) {
+export async function createUser(
+    email: string,
+    password: string,
+    username: string,
+    setLoading: (loading: boolean) => void,
+) {
     const { data, error } = await supabase.auth.signUp({
         email,
         password,
     });
 
-    if (error) throw error;
-    console.log('User created successfully');
+    if (error) {
+        setLoading(false);
+        Alert.alert('Error signing up', error.message);
+        return;
+    }
+    if (!data.session) {
+        setLoading(false);
+        Alert.alert('Please check your inbox for email verification!');
+        return;
+    }
     if (data.user) {
         await writeUserData({
             TableName: 'profiles',
@@ -17,19 +31,21 @@ export async function createUser(email: string, password: string, username: stri
     }
 }
 
-export async function login(email: string, password: string) {
+export async function login(
+    email: string,
+    password: string,
+    setLoading: (loading: boolean) => void,
+) {
     const { data, error } = await supabase.auth.signInWithPassword({
         email,
         password,
     });
 
-    if (error) throw error;
+    if (error) {
+        setLoading(false);
+        Alert.alert('Error logging in', error.message);
+        return;
+    }
     console.log('User logged in successfully');
-    return data.user;
-}
-
-export async function logout() {
-    const { error } = await supabase.auth.signOut();
-    if (error) throw error;
-    console.log('User logged out successfully');
+    return data;
 }
